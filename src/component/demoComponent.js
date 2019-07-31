6aac08b114af50890a6157aba71704614933d852
khan <khanmohammad2310@gmail.com>
2019-07-31 23:03:02 +0600

    add images

--- src/component/demoComponent.js
+++ src/component/demoComponent.js
@@ -1,10 +1,54 @@
 import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import image1 from "./image01.jpg";
+import image2 from "./image02.jpg";
+import image3 from "./image02.jpg";
 
-export default class DemoComponent extends Component {
+export default class SimpleSlider extends Component {
   render() {
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      vertical: false,
+      infinite: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000
+    };
     return (
-      <div className="demo-compo">
-        <h1>Hello React !</h1>
+      <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+            <img src={image1} alt="image" />
+          </div>
+          <div>
+            <h3>2</h3>
+            <img src={image2} alt="image" />
+          </div>
+          <div>
+            <h3>3</h3>
+            <img src={image3} alt="image" />
+          </div>
+          <div>
+            <h3>4</h3>
+            <img src={image1} alt="image" />
+          </div>
+          <div>
+            <h3>5</h3>
+            <img src={image2} alt="image" />
+          </div>
+          <div>
+            <h3>6</h3>
+            <img src={image3} alt="image" />
+          </div>
+        </Slider>
       </div>
     );
   }
